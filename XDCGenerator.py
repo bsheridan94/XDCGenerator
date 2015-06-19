5dc190298d3279322a60ed2878875d2c7d25847a
Brian <controler12@gmail.com>
2015-06-19 14:41:15 -0400

    added in base codes for switches. Needs fix for packing to make it look nicer but its getting packed in the Mall so im heading home.

--- XDCGenerator.py
+++ XDCGenerator.py
@@ -14,8 +14,8 @@ filemenu = Menu(menubar)
 root.title("Testing")
 
 
-w = Label(root, text="Hello, world!") 
-w.config(bg='black', fg='blue',cursor='gumby')
+w = Label(root, text="XDC Generator for Basys3") 
+w.config(bg='black', fg='white',cursor='gumby')
 w.pack()
 number = IntVar()
 
@@ -35,69 +35,38 @@ filemenu.add_command(label="Exit",command=quit)
 menubar.add_cascade(label="File", menu=filemenu) 
 root.config(menu=menubar) 
 
-quitButton = Tkinter.Button(root, text ="Quit", command = quit)
-B = Tkinter.Button(root, text ="Hello", command = helloCallBack)
-B.pack(side=TOP)
-quitButton.pack()
-
-def mouseEntered(event): 
-	button = event.widget 
-	button.config(text = "Please Please click me")
-
-def mouseExited(event): 
-	button = event.widget 
-	button.config(text = "Logon") 
-
-b = Button(root, text="Logon") 
-b.bind("<Enter>",mouseEntered) 
-b.bind("<Leave>",mouseExited) 
-b.pack()
-
-
 CheckVar1 = IntVar()
 CheckVar2 = IntVar()
-C1 = Checkbutton(root, text = "Check Me", variable = CheckVar1, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 10)
-C2 = Checkbutton(root, text = "Dont Check Me", variable = CheckVar2, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 10)
-C1.pack()
-C2.pack(side=TOP)
 
-listbox = Listbox(root)
+sw = [["sw[0]", IntVar(), 1, Entry(root), "V17"], ["sw[1]", IntVar(), 1, Entry(root), "V16"], ["sw[2]", IntVar(), 1, Entry(root), "W16"], ["sw[3]", IntVar(), 1, Entry(root), "W17"], ["sw[4]", IntVar(), 1, Entry(root), "W15"], ["sw[5]", IntVar(), 1, Entry(root), "V15"], ["sw[6]", IntVar(), 1, Entry(root), "W14"], ["sw[7]", IntVar(), 1, Entry(root), "W13"]]
 
 
-for i in range(5):
-    listbox.insert(END, str(i))
+for i in range(len(sw)):
+	print sw[i]
+	sw[i][2] = Checkbutton(root, text = sw[i][0], variable = sw[i][1], \
+                 onvalue = 1, offvalue = 0, height=1, \
+                 width = 4)
+	sw[i][2].pack()
+	sw[i][3].pack()
+
+	#print str(sw[i][3].get())
 
 
+def generateConstraintsFile():
+	ConstraintsFile = "MastaBasys1337.d\xdc" 
+	outputFile = open(ConstraintsFile, 'w')
+	outputFile.write("## switches")
 
-def callback():
-    if askyesno('Verify', 'Do you really want to quit?'):
-        showwarning('Yes', 'Quit not yet implemented')
-    else:
-        showinfo('No', 'Quit has been cancelled')
- 
-errmsg = 'Sorry, no Spam allowed!'
-Button(text='Quit', command=callback).pack(fill=X)
-Button(text='Spam', command=(lambda: showerror('Spam', errmsg))).pack(fill=X)
+	for i in range(len(sw)):
+		if sw[i][1].get() == 1:
+			outputFile.write("\nset_property PACKAGE_PIN "+ sw[i][4] + " [get_ports {" + sw[i][3].get() + 
+				"}]")
 
-e1 = Entry(root)
-e1.pack()
+generate = Button(text='Generate', command=generateConstraintsFile).pack(fill=X)
 
-
-text = Text(root)
-text.pack(side=TOP)
-
-
-def openFile():
-	choosen = askopenfilename(initialdir='~')
-	text.insert(END, open(choosen).read()) 
-
-
-quitButton = Tkinter.Button(root, text ="Open Text", command = openFile)
-quitButton.pack()
 
 #listbox.pack()
 root.mainloop()
+
+
+
